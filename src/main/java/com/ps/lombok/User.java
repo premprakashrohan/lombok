7fa289d8525fd0be895642504467c1d28bf46a84
Prem Prakash Rohan <prem.prakash.rohan@gmail.com>
2021-07-16 01:01:17 +0530

    Return this on setter

--- src/main/java/com/ps/lombok/User.java
+++ src/main/java/com/ps/lombok/User.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.experimental.Accessors;
 
 @Getter
 @Setter
@@ -14,19 +15,23 @@ public class User {
 	@Setter(AccessLevel.PRIVATE)
 	private Long id;
 
-	@Getter(AccessLevel.PROTECTED)
+	//@Getter(AccessLevel.PROTECTED)
+	@Accessors(fluent = true)
 	private String username;
 
-	//@Getter(AccessLevel.PACKAGE)
+	// @Getter(AccessLevel.PACKAGE)
 	private String email;
 
 	@Getter(AccessLevel.PUBLIC)
 	@Setter(AccessLevel.PRIVATE)
 	private LocalDate lastUpdated;
-	
-	public Optional<String> getEmail() {
-	    return Optional.ofNullable(email);
-	  }
 
+	public Optional<String> getEmail() {
+		return Optional.ofNullable(email);
+	}
+
+	public static void main(String[] args) {
+		User user = new User();
+		System.out.println(user==user.username(null));
+	}
 }
- 
