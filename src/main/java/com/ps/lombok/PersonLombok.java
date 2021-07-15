85134a1b70bd205dda83b8d95c779b9c2d5ed452
Prem Rohan <prerohan@in.ibm.com>
2021-07-15 14:31:26 +0530

    equals, hashCode and toString annotations It’s recommended to override the hashCode() and equals() methods while creating a class. In Lombok we have @EqualsAndHashCode annotation which injects code for equals() & hashCode() method as they go together. Additionally a @ToString annotation provides a toString() implementation. Let’s see this:
    @AllArgsConstructor @Getter @Setter
    @EqualsAndHashCode
    @ToString
    public class PersonLombok {
    	String firstName;
    	String lastName;
    	LocalDate dateOfBirth;
    }
    We now have achieved to create Person class without any boiler plate
    code with help of Lombok annotations. However it gets even better we can
    replace all the annotations used in the above class with @Data and get
    the same functionality.

--- src/main/java/com/ps/lombok/PersonLombok.java
+++ src/main/java/com/ps/lombok/PersonLombok.java
@@ -2,21 +2,26 @@ package com.ps.lombok;
 
 import java.time.LocalDate;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NonNull;
-import lombok.Setter;
+import lombok.*;
 
-@AllArgsConstructor @Getter @Setter
+@AllArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode
+@ToString
 public class PersonLombok {
-	@NonNull String firstName;
+	@NonNull
+	String firstName;
 	String lastName;
 	LocalDate dateOfBirth;
+
 	public static void main(String[] args) {
-	var person =	new PersonLombok("Prem", "Kumar", LocalDate.now());
-	System.out.println(person.getFirstName());
-	person.setFirstName("Rohan");
-	System.out.println(person.getFirstName());
-	 new PersonLombok(null, "Kumar", LocalDate.now());
+		var date = LocalDate.now();
+		var person = new PersonLombok("Prem", "Kumar", date);
+		System.out.println(person.getFirstName());
+		person.setFirstName("Rohan");
+		System.out.println(person.getFirstName());
+		System.out.println(person.equals(new PersonLombok("Rohan", "Kumar", date)));// true
+		new PersonLombok(null, "Kumar", LocalDate.now());
 	}
 }
