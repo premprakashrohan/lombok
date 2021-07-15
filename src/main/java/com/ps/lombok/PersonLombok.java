55c1a72b9797b4138096b85824a1d948817fcd7d
Prem Rohan <prem.prakash.rohan@gmail.com>
2021-07-15 14:27:23 +0530

    getter and setter

--- src/main/java/com/ps/lombok/PersonLombok.java
+++ src/main/java/com/ps/lombok/PersonLombok.java
@@ -3,14 +3,20 @@ package com.ps.lombok;
 import java.time.LocalDate;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NonNull;
+import lombok.Setter;
 
-@AllArgsConstructor
+@AllArgsConstructor @Getter @Setter
 public class PersonLombok {
 	@NonNull String firstName;
 	String lastName;
 	LocalDate dateOfBirth;
 	public static void main(String[] args) {
-		new PersonLombok(null, "Kumar", LocalDate.now());
+	var person =	new PersonLombok("Prem", "Kumar", LocalDate.now());
+	System.out.println(person.getFirstName());
+	person.setFirstName("Rohan");
+	System.out.println(person.getFirstName());
+	 new PersonLombok(null, "Kumar", LocalDate.now());
 	}
 }
