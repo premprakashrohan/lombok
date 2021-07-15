0fcdb5eff56b6596929840845dd91832323c8041
Prem Prakash Rohan <prem.prakash.rohan@gmail.com>
2021-07-16 01:54:35 +0530

    @Default

--- src/main/java/com/ps/lombok/Employee.java
+++ src/main/java/com/ps/lombok/Employee.java
@@ -3,19 +3,30 @@ package com.ps.lombok;
 import java.time.LocalDate;
 
 import lombok.Builder;
+import lombok.Builder.Default;
 import lombok.Data;
 
-@Builder @Data
+@Builder
+@Data
 public class Employee {
 	String firstName;
 	String lastName;
 	LocalDate dateOfBirth;
+	private Long id;
+	@Default
+	private String name = "anonymous";
+
+	private boolean active;
+
+	private int role;
+
+	private double salary;
+
+	@Default
+	private int defaultRole = 3;
 
 	public static void main(String[] args) {
-		Employee emp = new EmployeeBuilder().firstName("Rakesh")
-				.lastName("Kumar")
-				.dateOfBirth(LocalDate.now())
-				.build();
+		Employee emp = new EmployeeBuilder().firstName("Rakesh").lastName("Kumar").dateOfBirth(LocalDate.now()).build();
 		System.out.println(emp);
 	}
 }
